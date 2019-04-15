7ecb6e448cf917b8542905f29419e75382875999
Najib <najibslassi1996@gmail.com>
2019-04-15 17:44:36 +0200

    premier dépot

---
+++ src/com/excilys/cdb/mapper/DtoComputer.java
@@ -0,0 +1,12 @@
+package com.excilys.cdb.mapper;
+
+public class DtoComputer {
+	private Integer id;
+	private String name;
+	private String introduced;
+	private String discontinued;
+	private Integer companyId;
+	private Integer test;
+	public DtoComputer() {}
+
+}
